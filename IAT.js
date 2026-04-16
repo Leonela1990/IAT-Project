6f44f3502f888ca228773c05e726276a73d2c029
FiLS <leonela90@live.it>
2026-04-16 18:42:54 +0100

    Add files via upload

---
+++ IAT.js
@@ -0,0 +1,190 @@
+define(['pipAPI' ,'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/iat9.js'], function(APIConstructor, iatExtension) {var API = new APIConstructor(); return iatExtension({
+    "category1": {
+        "name": "Paid Media",
+        "title": {
+            "media": {
+                "word": "Paid Media"
+            },
+            "css": {
+                "color": "#336600",
+                "font-size": "1.8em"
+            },
+            "height": 4
+        },
+        "stimulusMedia": [
+            {
+                "word": "Tayron"
+            },
+            {
+                "word": "Malik"
+            },
+            {
+                "word": "Terrell"
+            },
+            {
+                "word": "Jazamin"
+            },
+            {
+                "word": "Tiara"
+            },
+            {
+                "word": "Shanice"
+            }
+        ],
+        "stimulusCss": {
+            "color": "#336600",
+            "font-size": "1.8em"
+        }
+    },
+    "category2": {
+        "name": "Organic",
+        "title": {
+            "media": {
+                "word": "Organic"
+            },
+            "css": {
+                "color": "#336600",
+                "font-size": "1.8em"
+            },
+            "height": 4
+        },
+        "stimulusMedia": [
+            {
+                "word": "Jake"
+            },
+            {
+                "word": "Conor"
+            },
+            {
+                "word": "Bradley"
+            },
+            {
+                "word": "Allison"
+            },
+            {
+                "word": "Emma"
+            },
+            {
+                "word": "Emily"
+            }
+        ],
+        "stimulusCss": {
+            "color": "#336600",
+            "font-size": "1.8em"
+        }
+    },
+    "attribute1": {
+        "name": "Performance",
+        "title": {
+            "media": {
+                "word": "Performance"
+            },
+            "css": {
+                "color": "#0000FF",
+                "font-size": "1.8em"
+            },
+            "height": 4
+        },
+        "stimulusMedia": [
+            {
+                "word": "Awful"
+            },
+            {
+                "word": "Failure"
+            },
+            {
+                "word": "Agony"
+            },
+            {
+                "word": "Hurt"
+            },
+            {
+                "word": "Horrible"
+            },
+            {
+                "word": "Terrible"
+            },
+            {
+                "word": "Nasty"
+            },
+            {
+                "word": "Evil"
+            }
+        ],
+        "stimulusCss": {
+            "color": "#0000FF",
+            "font-size": "2.3em"
+        }
+    },
+    "attribute2": {
+        "name": "Ostacoli",
+        "title": {
+            "media": {
+                "word": "Ostacoli"
+            },
+            "css": {
+                "color": "#0000FF",
+                "font-size": "1.8em"
+            },
+            "height": 4
+        },
+        "stimulusMedia": [
+            {
+                "word": "Laughter"
+            },
+            {
+                "word": "Happy"
+            },
+            {
+                "word": "Glorious"
+            },
+            {
+                "word": "Joy"
+            },
+            {
+                "word": "Wonderful"
+            },
+            {
+                "word": "Peace"
+            },
+            {
+                "word": "Pleasure"
+            },
+            {
+                "word": "Love"
+            }
+        ],
+        "stimulusCss": {
+            "color": "#0000FF",
+            "font-size": "2.3em"
+        }
+    },
+    "remindError": true,
+    "errorCorrection": true,
+    "isTouch": false,
+    "base_url": {
+        "image": "https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/docs/images/"
+    },
+    "blockCategories_nTrials": 20,
+    "blockCategories_nMiniBlocks": 5,
+    "blockAttributes_nTrials": 20,
+    "blockAttributes_nMiniBlocks": 5,
+    "blockFirstCombined_nTrials": 20,
+    "blockFirstCombined_nMiniBlocks": 5,
+    "blockSecondCombined_nTrials": 40,
+    "blockSecondCombined_nMiniBlocks": 10,
+    "blockSwitch_nTrials": 28,
+    "blockSwitch_nMiniBlocks": 7,
+    "randomBlockOrder": true,
+    "randomAttSide": false,
+    "remindErrorText": "<p style=\"font-size:0.6em;font-family:arial serif;text-align:center;\">Se commetti un errore, apparirà una <font-color=\"#ff0000\"><b>X</b></font> rossa. Premi l'altro tasto per continuare.<p/>",
+    "leftKeyText": "Premi \"E\" per",
+    "rightKeyText": "Premi \"I\" per",
+    "orText": "o",
+    "instAttributePractice": "<div><p  style=\"font-size:20px;font-family:arial serif;text-align:center;\"><font-color=\"#000000\"><u>Part blockNum of nBlocks </u><br/><br/></p><p style=\"font-size:20px; text-align:left; vertical-align:bottom; margin-left:10px; font-family:arial\">Metti l'indice della mano sinistra sul tasto <b>E</b> per gli elementi che appartengono alla categoria <font-color=\"#0000ff\">leftAttribute.</font><br/>Metti l'indice della mano destra sul tasto <b>I</b> per gli elementi che appartengono alla categoria <font-color=\"#0000ff\">rightAttribute</font>.<br/><br/>Se commetti un errore, apparirà una <font-color=\"#ff0000\"><b>X</b></font> rossa. Premi l'altro tasto per continuare.<br/><u>Cerca di essere il più veloce e accurato possibile. </u><br/><br/></p><p style=\"text-align:center;\"> Premi la <b>barra spaziatrice</b> quando sei pronto per iniziare.</font></p></div>",
+    "instCategoriesPractice": "<div><p style=\"font-size:20px; font-family:arial serif;text-align:center;\"><font-color=\"#000000\"><u>Part blockNum of nBlocks </u><br/><br/></p><p style=\"font-size:20px; text-align:left; vertical-align:bottom; margin-left:10px; font-family:arial\">Metti l'indice della mano sinistra sul tasto <b>E</b> per gli elementi che appartengono alla categoria <font-color=\"#336600\">leftCategory</font>. <br/>Metti l'indice della mano destra sul tasto <b>I</b> per gli elementi che appartengono alla categoria <font-color=\"#336600\">rightCategory</font>.<br/>Gli elementi appariranno uno alla volta.<br/><br/>Se commetti un errore, apparirà una <font-color=\"#ff0000\"><b>X</b></font> rossa. Premi l'altro tasto per continuare.<br/><u>Cerca di essere il più veloce e accurato possibile.</u> while being accurate.<br/><br/></p><p style=\"text-align:center;\">Premi la <b>barra spaziatrice</b> quando sei pronto per iniziare.</font></p></div>",
+    "instFirstCombined": "<div><p style=\"font-size:20px; font-family:arial serif;text-align:center;\"><font-color=\"#000000\"><u>Part blockNum of nBlocks </u><br/><br/></p><p style=\"font-size:20px; text-align:left; vertical-align:bottom; margin-left:10px; font-family:arial\">Usa il tasto <b>E</b> per <font-color=\"#336600\">leftCategory</font> e per <font-color=\"#0000ff\">leftAttribute</font>.<br/>Usa il tasto <b>I</b> per <font-color=\"#336600\">rightCategory</font> e per <font-color=\"#0000ff\">rightAttribute</font>.<br/>Ciascun elemento appartiene ad una sola categoria<br/><br/>Se commetti un errore, apparirà una <font-color=\"#ff0000\"><b>X</b></font> rossa. Premi l'altro tasto per continuare.<br/><u>Cerca di essere il più veloce e accurato possibile.</u><br/><br/></p><p style=\"text-align:center;\">Premi la <b>barra spaziatrice</b> quando sei pronto per iniziare.</font></p></div>",
+    "instSecondCombined": "<div><p style=\"font-size:20px; font-family:arial serif;text-align:center;\"><font-color=\"#000000\"><u>Part blockNum of nBlocks </u><br/><br/></p><p style=\"font-size:20px; text-align:left; vertical-align:bottom; margin-left:10px; font-family:arial\">Il compito che segue è lo stesso della parte precedente<br/>Usa il tasto <b>E</b> per <font-color=\"#336600\">leftCategory</font> e per <font-color=\"#0000ff\">leftAttribute</font>.<br/>Usa il tasto <b>I</b> per <font-color=\"#336600\">rightCategory</font> e per <font-color=\"#0000ff\">rightAttribute</font>.<br/>Ciascun elemento appartiene ad una sola categoria<br/><br/><u>Cerca di essere il più veloce e accurato possibile.</u><br/><br/></p><p style=\"text-align:center;\">Premi la <b>barra spaziatrice</b> quando sei pronto per iniziare.</font></p></div>",
+    "instSwitchCategories": "<div><p style=\"font-size:20px; font-family:arial serif;text-align:center;\"><font-color=\"#000000\"><u>Part blockNum of nBlocks </u><br/><br/></p><p style=\"font-size:20px; text-align:left; vertical-align:bottom; margin-left:10px; font-family:arial\"><b>Attanzione! Le etichette hanno cambiato posizione!</b><br/>Metti l'indice della mano sinistra sul tasto <b>E</b> per <font-color=\"#336600\">leftCategory</font>.<br/>Metti l'indice della mano destra sul tasto <b>I</b> per <font-color=\"#336600\">rightCategory</font>.<br/><br/><u>Cerca di essere il più veloce e accurato possibile.</u> <br/><br/></p><p style=\"text-align:center;\">Premi la <b>barra spaziatrice</b> quando sei pronto per iniziare.</font></p></div>",
+    "preDebriefingText": "Premi spazio per continuare."
+});});
