c9d976784e51859af59fa036668d5a0c9cc84c78
FiLS <leonela90@live.it>
2026-04-17 13:10:47 +0100

    Update ringraziamento.js

--- ringraziamento.js
+++ ringraziamento.js
@@ -1,35 +1,28 @@
 define(['questAPI'], function(Quest){
+    var API = new Quest();
 
-  var API = new Quest();
-
-  /**
-   * Page prototype
-   */
-  API.addPagesSet('basicPage',{
-		noSubmit:'true',
-		v1style: 2,
-		header: 'Hai completato lo studio. Grazie per la tua partecipazione!',
-		decline: 'false',
-		autoFocus: 'true'
-	});
-  
     /**
-   * Question prototypes
-   */
-   API.addQuestionsSet('basicSelect',{
-    type: 'selectOne',
-    autoSubmit:'true',
-    maxWidth: '60%'
-  });
-  
- return API.script;
+    * Page prototype
+    */
+    API.addPagesSet('basicPage',{
+        noSubmit: true, 
+        v1style: 2,
+        header: 'Hai completato lo studio. Grazie per la tua partecipazione!',
+        decline: false,
+        autoFocus: true 
+    });
+
+    API.addSequence([
+        {
+            inherit: 'basicPage',
+            questions: [
+                {
+                    type: 'info',
+                    stem: 'I tuoi dati sono stati salvati correttamente. Puoi chiudere la finestra.'
+                }
+            ]
+        }
+    ]);
+
+    return API.script;
 });
-
-
-
-
-
-
-
-
-
